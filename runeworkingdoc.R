06618fb98586b22ce962b394c7cb7c97089e7a14
Rune-Nedergaard <76943893+Rune-Nedergaard@users.noreply.github.com>
2022-01-08 17:50:10 +0100

    Har kigget lidt på multinomial logistic regression og test/train split

--- runeworkingdoc.R
+++ runeworkingdoc.R
@@ -1,6 +1,7 @@
 #load data
 load('armdata.RData')
 
+### below is taken from: https://gitlab.gbar.dtu.dk/s183897_s183926_s183924/Project_1 ###
 data <- as.matrix(armdata)
 
 
@@ -55,4 +56,66 @@ interaction.plot(s$Exp, s$Per, s$height)
 
 # P-adjust using method
 p.adjust(c(2.2e-16,2.2e-16), method = 'hochberg')
+
+
+####### Logistic regresssion ############
+#taken from:https://github.com/StatQuest/logistic_regression_demo/blob/master/logistic_regression_demo.R
+
+library(ggplot2)
+library(cowplot)
+
+head(s)
+
+str(s)
+
+nrow(s)
+
+
+
+############### Test  Train split ######################
+
+smp_size <- floor(0.75 * nrow(s))
+
+
+set.seed(42)
+
+
+train_ind <- sample(seq_len(nrow(s)), size = smp_size)
+
+train <- s[train_ind, ]
+test <- s[-train_ind, ]
+
+#####################################
+
+
+###### Multinomial logistic regression ##########
+
+#https://datasciencebeginners.com/2018/12/20/multinomial-logistic-regression-using-r/ 
+
+require(nnet)
+
+# Setting the baseline 
+train$Per <- relevel(train$Per, ref = "1")
+
+#multinom.fit <- multinom(Per ~ height, data=train)
+
+multinom.fit <- multinom(Per ~ ., data=train)
+
+
+
+summary(multinom.fit)
+
+#############
+
+# Predicting the values for train dataset
+train$precticed <- predict(multinom.fit, newdata = train, "class")
+
+# Building classification table
+ctable <- table(train$Per, train$precticed)
+
+# Calculating accuracy - sum of diagonal elements divided by total obs
+round((sum(diag(ctable))/sum(ctable))*100,2)
+
+
+
 
