a7aea27f726b721f0b66236f3a15195cab46edb1
Andreas Fiehn <andreas@lyhnefiehn.dk>
2022-01-09 11:58:54 +0100

    Visualize data

---
+++ viasualization.R
@@ -0,0 +1,19 @@
+library(rgl)
+
+# Load data
+load("armdata.RData")
+
+
+# Code taken from project description
+start_cyl <- cylinder3d(cbind(0, 0, seq(0, 10, length = 10)), radius = c(3,3,3), sides = 20, closed = -2)
+target_cyl <- cylinder3d(cbind(60, 0, seq(0, 10, length = 10)), radius = c(3,3,3), sides = 20, closed = -2)
+cyl1 <- cylinder3d(cbind(0, 0, 10 + seq(0, 12.5, length = 10)), radius = c(3,3,3), sides = 20, closed = -2)
+cyl2 <- cylinder3d(cbind(60, 0, 10 + seq(0, 12.5, length = 10)), radius = c(3,3,3), sides = 20, closed = -2)
+cyl3 <- cylinder3d(cbind(30, 0, seq(0, 20, length = 10)), radius = c(3,3,3), sides = 10, closed = -2)
+shade3d(addNormals(subdivision3d(start_cyl)), col = 'darkgreen')
+shade3d(addNormals(subdivision3d(target_cyl)), col = 'darkgreen')
+shade3d(addNormals(subdivision3d(cyl1)), col = 'pink')
+shade3d(addNormals(subdivision3d(cyl2)), col = 'pink', alpha = 0.5)
+shade3d(addNormals(subdivision3d(cyl3)), col = 'lightblue')
+surface3d(c(-7, 67), c(-20, 20), matrix(0, 2, 2), col = "brown", alpha = 0.9, specular = "black")
+lines3d(armdata[[7]][[1]][[1]])
