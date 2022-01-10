a2260135d79f900593b2373e03dbf9330a85d28e
Andreas Fiehn <andreas@lyhnefiehn.dk>
2022-01-10 11:49:17 +0100

    Calculate sum of distance between all points in curve and make boxplots

--- Projekt.R
+++ Projekt.R
@@ -1,13 +1,17 @@
 # Load cleaned data 
 armdata <- readRDS("armdata_cleaned.rds")
 
-
 # Create jpg file path for plots by providing a plot name
 getJpgFilePath <- function(plot_name) {
   plots_path <- "plots"
   file_type <- "jpg"
   path <- paste(paste(plots_path, plot_name, sep="/"), file_type, sep=".")
   
+}
+
+getDist3d <- function(v1, v2) {
+  dist <- sqrt((v2[1] - v1[1])^2 + (v2[2]- v1[2])^2 + (v2[3]- v1[3])^2)
+  return(dist)
 }
 
 # =======================================================
@@ -80,28 +84,38 @@ for (i in 1:n_experiments){
       # Define obstacle height
       obstacle_height <- exp_setups[[i]][[2]]
       
-      paths <- rbind(paths, c(i, d, obstacle_height, j, k, path_height, z_vertex, x_vertex, y_range, y_std, z_min))
+
+      
+      
+      curve_dist <- 0
+      prev_point <- NA
+      # Loop through all points and sum distance between points
+      for (n in 1:nrow(repetition)){
+        if (!is.na(sum(prev_point))){
+          curve_dist <- curve_dist + getDist3d(repetition[n,], prev_point)    
+        }
+        
+        prev_point <- repetition[n,]
+      }
+      
+      
+      paths <- rbind(paths, c(i, d, obstacle_height, j, k, path_height, z_vertex, x_vertex, y_range, y_std, z_min, curve_dist))
 
     }
   }
 }
 
-
 # Crate dataframe from matrix
 paths <- data.frame(paths)
 
 # Add column names
-colnames(paths) <- c("Experiment", "d" , "obstacleHeight", "Person", "Repetition", "pathHeight", "zVertex", "xVertex", "yRange", "yStd", "zMin")
+colnames(paths) <- c("Experiment", "d" , "obstacleHeight", "Person", "Repetition", "pathHeight", "zVertex", "xVertex", "yRange", "yStd", "zMin", "pathDist")
 head(paths)
 
 # Define variables as factors
-s$Experiment <- as.factor(s$Experiment)
-s$Person <- as.factor(s$Person)
-s$Repetition <- as.factor(s$Repetition)
-
-# We consider obstacleHeight and d as nominal data
-s$obstacleHeight <- as.factor(s$obstacleHeight)
-s$d <- as.factor(s$d)
+paths$Experiment <- as.factor(paths$Experiment)
+paths$Person <- as.factor(paths$Person)
+paths$Repetition <- as.factor(paths$Repetition)
 
 
 jpeg(file= getJpgFilePath("boxplot_pathHeight_obstacleHeight"))
@@ -112,12 +126,94 @@ jpeg(file= getJpgFilePath("boxplot_xVertex_d"))
 boxplot(paths$xVertex ~ paths$d, ylab="x vertex", xlab="d")
 dev.off()
 
+jpeg(file= getJpgFilePath("boxplot_pathHeight_person"))
+boxplot(paths$pathHeight ~ paths$Person, ylab="path distance", xlab="Person")
+dev.off()
 
-L <- lm(obstacleHeight ~ pathHeight, data = paths)
-anova(L)
-L <- lm(obstacleHeight ~ xVertex, data = paths)
-anova(L)
+jpeg(file= getJpgFilePath("boxplot_pathDist_person"))
+boxplot(paths$pathDist ~ paths$Person, ylab="path distance", xlab="Person")
+dev.off()
 
-# Check for interaction
-L <- lm(obstacleHeight ~ pathHeight * xVertex, data = paths)
-anova(L)
+# L <- lm(pathHeight ~ . , data = paths)
+# anova(L)
+
+# interaction.plot(paths$Experiment, paths$Person, paths$pathHeight)
+# 
+# anv <- aov(pathHeight ~ Person + Experiment, data = paths)
+# plot(anv)
+# 
+# # L <- lm(paths$pathHeight ~ paths$Person , data = paths)
+# anova(L)
+
+# 
+# L <- lm(paths$xVertex ~ paths$Person , data = paths)
+# anova(L)
+# 
+# 
+# plot(L)
+# 
+# summary(L)
+# 
+# 
+# 
+# s <- paths[paths$Experiment == 2,]
+# 
+# 
+# 
+# # path height
+# boxplot(s$pathHeight ~ s$Person, ylab="path height", xlab="Person")
+# 
+# kruskal.test(s$pathHeight, s$Person)
+# 
+# pairwise.wilcox.test(s$pathHeight, s$Person,
+#                      p.adjust.method = "BH")
+# 
+# 
+# 
+# 
+# # path height
+# boxplot(paths$pathHeight ~ paths$Person, ylab="path height", xlab="Person")
+# 
+# kruskal.test(paths$pathHeight, paths$Person)
+# 
+# pairwise.wilcox.test(paths$pathHeight, paths$Person,
+#                      p.adjust.method = "BH")
+# 
+# 
+# # x-coordinate, toppunkt
+# boxplot(paths$xVertex ~ paths$Person, ylab="xVertex", xlab="Person")
+# 
+# kruskal.test(paths$xVertex, paths$Person)
+# 
+# pairwise.wilcox.test(paths$xVertex, paths$Person,
+#                      p.adjust.method = "BH")
+# 
+# 
+# # yRange
+# boxplot(paths$yRange ~ paths$Person, ylab="yRange", xlab="Person")
+# 
+# kruskal.test(paths$yRange, paths$Person)
+# 
+# pairwise.wilcox.test(paths$yRange, paths$Person,
+#                      p.adjust.method = "BH")
+# 
+# 
+# 
+# 
+# 
+# pairwise
+# 
+# ?kruskal.test
+# 
+# L <- lm(obstacleHeight ~ pathHeight, data = paths)
+# anova(L)
+# 
+# 
+# predict.lm()
+# 
+# L <- lm(obstacleHeight ~ xVertex, data = paths)
+# anova(L)
+# 
+# # Check for interaction
+# L <- lm(obstacleHeight ~ pathHeight * xVertex, data = paths)
+# anova(L)
